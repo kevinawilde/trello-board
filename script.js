c81cb8bc094876ac4401e7f765bed0d14185e31a
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-05-01 19:41:35 -0500

    dragability added

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
+var cardCount = 1;
 function allowDrop(ev) {
  	ev.preventDefault();
 }
 
 function drag(ev) {
- 	ev.dataTransfer.setData("text", ev.target.id);
+ 	ev.dataTransfer.setData("text/html", ev.target.id);
 }
 
 function drop(ev) {
 	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
+	var data = ev.dataTransfer.getData("text/html");
 	ev.target.appendChild(document.getElementById(data));
 }
 function createPopover(){
 	document.getElementById('popup').style.display='block';
 }
 function createTask(){
+	cardCount++
 	/*Create Card*/
 	var card = document.createElement('div');
+	card.setAttribute("draggable", "true");
+	card.setAttribute("ondragstart", "drag(event)");
 	card.className = 'card';
+	card.id=`taskCard${cardCount}`
 	var taskName=document.getElementById("taskName").value;
 	var taskBody=document.getElementById("description").value;
 	/*card.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000';*/
-	document.getElementById('cards').appendChild(card);
+	
 	/*Card Head*/
 	var cardHead = document.createElement('div');
 	var cardHeadText = document.createTextNode(taskName); 
@@ -32,9 +37,10 @@ function createTask(){
 	var cardBody = document.createElement('div');
 	
 	cardBody.className = 'card-body';
-	var cardBodyText = document.createTextNode(taskName); 
+	var cardBodyText = document.createTextNode(taskBody); 
 	cardBody.appendChild(cardBodyText);
 	card.appendChild(cardBody);
+	document.getElementById('cards').appendChild(card);
 
 }
 function cancelTask(){
