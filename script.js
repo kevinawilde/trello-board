bf142ed7e965f432f009e30122b940072545ca3d
Kevin Wilde <kevin.a.wilde@gmail.com>
2019-05-06 17:12:38 -0500

    add list done, edit list name done for existing lists

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var cardCount = 1;
+
 function allowDrop(ev) {
  	ev.preventDefault();
 }
@@ -69,8 +70,69 @@ function createTask(){
 
 	document.getElementById('cards').appendChild(card);
 	document.getElementById('popup').style.display='none';
+	document.getElementById("taskName").value = '';
+	document.getElementById("description").value = '';
+
+}
+function createList(){
+	var row = document.getElementById("lists")
+	/*List Creation*/
+	var list = document.createElement('div');
+	list.className = 'col';
+	list.setAttribute("ondrop", "drop(event)");
+	list.setAttribute("ondragover", "allowDrop(event)");
+	row.appendChild(list);
+	/*Header*/
+	var listName = document.createElement('h3');
+	list.appendChild(listName);
+	var listNameText = document.getElementById("listName").value;
+	var listNameNode = document.createTextNode(listNameText);
+	listName.appendChild(listNameNode);
+	var editButton = document.createElement('a');
+	editButton.setAttribute("href", "#");
+	editButton.setAttribute("onclick", "edit(this)");
+	editButton.className = "newListButton"
+	listName.appendChild(editButton);
+	var editButtonImg = document.createElement('img');
+	editButtonImg.setAttribute("src", "edit-icon.png");
+	editButton.appendChild(editButtonImg);
+
+	document.getElementById('createListPopover').style.display='none';
+	document.getElementById("listName").value = '';
 
 }
 function cancelTask(){
 	document.getElementById('popup').style.display='none';
+	document.getElementById("taskName").value = '';
+	document.getElementById("description").value = '';
+}
+function createListPopover(){
+	document.getElementById('createListPopover').style.display='block';
+}
+function cancelList(){
+	document.getElementById('createListPopover').style.display='none';
+	document.getElementById("listName").value = '';
+}
+function edit(a){
+	document.getElementById("editName").style.display = "block";
+	var idValue = a.parentElement.parentElement.id;
+	document.getElementById("valueHolder").value = idValue;
+	var currentListName = a.parentElement.textContent;
+	document.getElementById("editListName").setAttribute("placeholder", currentListName);
+
+}
+function cancelListEdit(){
+	document.getElementById("editName").style.display = "none";
+}
+function editList(){
+	var getIdValue = document.getElementById("valueHolder").value;
+	var newListName = document.getElementById("editListName").value;
+	var valueId = document.getElementById(getIdValue);
+	document.getElementById("editListName").setAttribute("placeholder", valueId);
+	valueId.getElementsByTagName("h3")[0].innerHTML = newListName;
+
+
+}
+function cancelEditList(){
+
 }
